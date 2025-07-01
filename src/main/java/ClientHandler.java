fe7161713369685c48db25583db170e5be4c15d1
pragya sharma <pragyasharma1209@gmail.com>
2025-07-01 10:57:04 +0530

    handle expiry code

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -84,10 +84,9 @@ class ClientHandler extends Thread {
     }
 
     private void handleSetCommand(BufferedReader in, OutputStream out) throws IOException {
-        // Read the parameters of the SET command
-        List<String> params = new ArrayList<>();
-        params.add(readArgument(in));  // Key
-        params.add(readArgument(in));  // Value
+        // Read the key and value for the SET command
+        String key = readArgument(in);  // The key
+        String value = readArgument(in);  // The value
 
         // Default expiration is 0 (no expiration)
         long expiryTimeMillis = 0;
@@ -99,20 +98,19 @@ class ClientHandler extends Thread {
             // If it is "px", read the expiration time in milliseconds
             expiryTimeMillis = Long.parseLong(readArgument(in));  // Expiry in milliseconds
         } else {
-            // If no "px" argument, we push it back to params list
-            params.add(nextArg);  // Non-"px" argument gets added back
+            // If no "px" argument, just continue with key-value
+            // We can ignore this extra argument for now (it will be null in case of no "px")
         }
 
         // Calculate the expiration timestamp (if expiration time is specified)
         long expirationTimestamp = expiryTimeMillis > 0 ? System.currentTimeMillis() + expiryTimeMillis : 0;
 
         // Store the key-value pair with expiration (if any)
-        keyValueStore.put(params.get(0), new KeyValue(params.get(1), expirationTimestamp));
+        keyValueStore.put(key, new KeyValue(value, expirationTimestamp));
 
         // Respond with +OK for SET command
         out.write("+OK\r\n".getBytes());
     }
-
 
 
     private void handleGetCommand(BufferedReader in, OutputStream out) throws IOException {
