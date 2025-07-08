999cb016d4b63bf0b8200316d6ebc51fa7a8e8df
pragya sharma <pragyasharma1209@gmail.com>
2025-07-08 10:47:39 +0530

    expiry code

--- src/main/java/RDBParser.java
+++ src/main/java/RDBParser.java
@@ -26,6 +26,9 @@ public class RDBParser {
             switch (b) {
                 case 0xFD:
                 	expireAtMillis = readUnsignedLong(in);
+                	if (expireAtMillis < 2_000_000_000_000L) { // before ~2033 in millis
+                        expireAtMillis *= 1000;
+                    }
                     hasExpiry = true;
                     break;
                 case 0xFC:
