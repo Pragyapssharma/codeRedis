ba283593de94290fb1eb8f165fcae23828d220fd
pragya sharma <pragyasharma1209@gmail.com>
2025-07-10 10:12:05 +0530

    expiry handle code

--- src/main/java/RDBParser.java
+++ src/main/java/RDBParser.java
@@ -32,10 +32,10 @@ public class RDBParser {
 			case 0xFC:
 				byte[] expiryBytes = new byte[4];
 				in.readFully(expiryBytes);
-				long ttlSeconds = ((expiryBytes[0] & 0xFFL) << 24) | ((expiryBytes[1] & 0xFFL) << 16)
+				long expirySeconds = ((expiryBytes[0] & 0xFFL) << 24) | ((expiryBytes[1] & 0xFFL) << 16)
 			            | ((expiryBytes[2] & 0xFFL) << 8) | (expiryBytes[3] & 0xFFL);
-			    expireAtMillis = System.currentTimeMillis() + (ttlSeconds * 1000L);
-				System.out.printf("Expiry (0xFC) seconds: %d, millis: %d\n", ttlSeconds, expireAtMillis);
+			    expireAtMillis = expirySeconds * 1000L;
+				System.out.printf("Expiry (0xFC) seconds: %d, millis: %d\n", expirySeconds, expireAtMillis);
 				hasExpiry = true;
 				break;
 
